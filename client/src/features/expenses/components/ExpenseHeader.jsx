2b9a7be2ae59824236747dd67e34898b65e397ee
zahraai3 <zahraaibrahem241@gmail.com>
2026-07-27 00:52:46 +0300

    refactor: changed user greeting in the header

--- client/src/features/expenses/components/ExpenseHeader.jsx
+++ client/src/features/expenses/components/ExpenseHeader.jsx
@@ -1,11 +1,9 @@
-import { useUserData } from '../../auth/hooks/useUserData';
-import { useAuth } from '../../auth/context/authContext';
+
 import {Icon} from "@iconify/react";
 import styles from './ExpenseHeader.module.css'
 
 export function ExpenseHeader(){
-    const {user} = useAuth()
-    const {data:userData } = useUserData(user.uid)
+    
     
     return(
         <div className={styles.header}>
@@ -16,7 +14,7 @@ export function ExpenseHeader(){
                 />
                 <h1>Dashboard</h1>
             </div>
-            <h2>Hello Dear {userData?.displayName}</h2>
+            <h2></h2>
         </div>
     )
 
--- client/src/shared/components/HeaderNav.jsx
+++ client/src/shared/components/HeaderNav.jsx
@@ -1,10 +1,15 @@
 import styles from './HeaderNav.module.css';
+import { useUserData } from '../../features/auth/hooks/useUserData';
+import { useAuth } from '../../features/auth/context/authContext';
 
 const HeaderNav = () => {
+
+    const {user} = useAuth()
+    const {data:userData } = useUserData(user.uid)
     return (
         <header className={styles['header']}>
         <h1 className={styles['header_title']}>HomeHub</h1>
-        <p className={styles['header_tagline']}>Manage your home, together.</p>
+        <p className={styles['header_tagline']}>Hello Dear {userData?.displayName}</p>
         </header>
     );
 };
